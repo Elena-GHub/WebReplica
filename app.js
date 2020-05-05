c4ef65848251bf7a9fcdb2692b8a79d46369dbb6
Elena <60514098+Elena-GHub@users.noreply.github.com>
2020-05-05 18:06:13 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -12,47 +12,18 @@ app.engine('handlebars', handlebars({
 
 app.use(express.static('public'));
 
-const mascota = () => {
-    return [
-        {
-        name: 'Thundercat',
-        class: 'dog',
-        class2: 'fas fa-dog',
-        human: 'Edgar'
-        },
-        {
-        name: 'Greta',
-        class: 'dog',
-        class2: 'fas fa-dog',
-        human: 'Diego'
-        },
-        {
-        name: 'Chuches',
-        class: 'dog',
-        class2: 'fas fa-dog',
-        human: 'David'
-        },
-        {
-        name: 'Pol',
-        class: 'cat',
-        class2: 'fas fa-cat',
-        human: 'Eva'
-        },
-        {
-        name: 'Nanu',
-        class: 'dog',
-        class2: 'fas fa-dog',
-        human: 'Javi'
-        }
-    ]
-    }
-
-app.get('/', (req, res) => {
-    res.render('main', { layout: 'index', bootcodersPets: mascota()});
-});
 
 app.listen(port, () => {
     console.log('App listening to port ${port}');
 });
 
-
+$(document).ready(function ()
+{
+    $.ajax(./contents/mascotas.json).done(function (mascota)
+    {
+        app.get('/', (req, res) =>
+        {
+            res.render('main', { layout: 'index', mascotas: mascota() });
+        });
+    });
+});
